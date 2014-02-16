8e9885be9166426eae47b956fa5d1651ada03216
shazzl <rizalshahrin@gmail.com>
2014-02-17 02:36:52 +0800

    revert gpu oc

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3573,8 +3573,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 	F_GFX3D(266667000, pll2,  1,  3),
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  2,  5),
-	F_GFX3D(487500000, pll2,  2,  5),
-	F_GFX3D(533333000, pll15, 1,  2),
+	F_GFX3D(500000000, pll15, 1,  2),
 	F_END
 };
 
@@ -3605,7 +3604,7 @@ static unsigned long fmax_gfx3d_8930aa[VDD_DIG_NUM] = {
 static unsigned long fmax_gfx3d_8930ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 192000000,
 	[VDD_DIG_NOMINAL] = 320000000,
-	[VDD_DIG_HIGH]    = 533333000
+	[VDD_DIG_HIGH]    = 500000000
 };
 
 static struct bank_masks bmnd_info_gfx3d = {
